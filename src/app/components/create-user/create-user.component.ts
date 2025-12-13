f01d27735e9d92c2416808da7f290b74508eb92f
JavierMolines <eljavimolines@gmail.com>
2025-12-13 05:53:01 -0300

    feat: implement reactive form for player input validation in CreateUser component

--- src/app/components/create-user/create-user.component.ts
+++ src/app/components/create-user/create-user.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-create-user',
-  imports: [RouterModule],
+  imports: [RouterModule, ReactiveFormsModule],
   templateUrl: './create-user.component.html',
   styleUrl: './create-user.component.css',
 })
 export class CreateUserComponent {
+  mininumPlayersValids = 2;
+  validToStartGame = signal(false);
+  formPlayers: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.formPlayers = this.fb.group({
+      player1: [''],
+      player2: [''],
+      player3: [''],
+      player4: ['']
+    });
+  }
+
+  validateInputsContent() {
+    const inputs: Array<string> = Object.values(this.formPlayers.value);
+    const inputsWithText = inputs.filter(text => text && text.trim() !== '' && text.length > 2);
+    this.validToStartGame.set(inputsWithText.length >= this.mininumPlayersValids);
+  }
 }
