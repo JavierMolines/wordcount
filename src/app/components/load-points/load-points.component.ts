31df35aa46740f11eb16792aca8516026d873ae6
JavierMolines <eljavimolines@gmail.com>
2025-12-13 10:46:38 -0300

    feat: implement load points form with validation and player selection

--- src/app/components/load-points/load-points.component.ts
+++ src/app/components/load-points/load-points.component.ts
@@ -1,10 +1,48 @@
-import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { Component, OnInit, signal, WritableSignal } from "@angular/core";
+import {
+	FormBuilder,
+	FormGroup,
+	ReactiveFormsModule,
+	Validators,
+	ɵInternalFormsSharedModule,
+} from "@angular/forms";
 import { RouterModule } from "@angular/router";
+import { loadPlayers } from "../../helper/storage.helper";
 
 @Component({
 	selector: "app-load-points",
-	imports: [RouterModule],
+	imports: [
+		RouterModule,
+		CommonModule,
+		ɵInternalFormsSharedModule,
+		ReactiveFormsModule,
+	],
 	templateUrl: "./load-points.component.html",
 	styleUrl: "./load-points.component.css",
 })
-export class LoadPointsComponent {}
+export class LoadPointsComponent implements OnInit {
+	players: WritableSignal<Array<string>> = signal([]);
+	form: FormGroup;
+
+	constructor(private fb: FormBuilder) {
+		this.form = this.fb.group({
+			item1: ["", Validators.required],
+			item2: ["", Validators.required],
+			select: ["", Validators.required],
+		});
+	}
+
+	ngOnInit(): void {
+		const players = loadPlayers();
+		this.players.set(players);
+	}
+
+	onClickButton(): void {
+		if (!this.form.valid) {
+			return;
+		}
+
+		console.log("Button clicked");
+	}
+}
